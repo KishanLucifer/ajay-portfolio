2f231954cdfa07e4e43c2817b4f91f8112f6528d
kishan <kishanrokk@gmail.com>
2024-07-08 16:27:06 +0530

    update 1

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
 import { useForm } from "react-hook-form";
-import { PageInfo } from "@/typings";
+import { PageInfo, Social } from "@/typings";
+import { SocialIcon } from "react-social-icons";
 
-type Props = { pageInfo: PageInfo };
+type Props = { pageInfo: PageInfo; socials: Social[] };
 
 type Inputs = {
   name: string;
@@ -12,7 +13,7 @@ type Inputs = {
   message: string;
 };
 
-function ContactMe({ pageInfo }: Props) {
+function ContactMe({ pageInfo, socials }: Props) {
   const { register, handleSubmit } = useForm<Inputs>();
   const onSubmit = (formData: Inputs) => {
     const emailBody = `Hi, my name is ${formData.name}. ${formData.message}\n(${formData.email})`;
@@ -24,40 +25,41 @@ function ContactMe({ pageInfo }: Props) {
     }
   };
   return (
-    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly lg:mx-auto items-center">
-      <h3 className="absolute ml-6 lg:ml-10 top-24 uppercase tracking-[20px] text-md text-gray-500">
-        Contact
-      </h3>
+    <>
+      <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly lg:mx-auto items-center">
+        <h3 className="absolute ml-6 lg:ml-10 top-24 uppercase tracking-[20px] text-md text-gray-500">
+          Contact
+        </h3>
 
-      <div className="flex flex-col space-y-6 mt-32">
-        {/* <h4 className="text-lg  sm:text-3xl md:text-4xl xl:text-4xl font-semibold text-center">
+        <div className="flex flex-col space-y-6 mt-32">
+          {/* <h4 className="text-lg  sm:text-3xl md:text-4xl xl:text-4xl font-semibold text-center">
           I have got just what you need.{" "}
           <span className="underline decoration-[#1da1f2]">Lets Talk </span>
         </h4> */}
 
-        <div className="flex flex-col space-y-5">
-          <div
-            className="flex items-center space-x-3 justify-center"
-            onClick={handlePhoneNumberClick}
-          >
-            {/* <PhoneIcon className="text-sky-400 h-4 w-4 animate-pulse" /> */}
-            <p className="text-sm md:text-xl xl:text-lg">
-              {pageInfo.phoneNumber}
-            </p>
-          </div>
+          <div className="flex flex-col space-y-5">
+            <div
+              className="flex items-center space-x-3 justify-center"
+              onClick={handlePhoneNumberClick}
+            >
+              {/* <PhoneIcon className="text-sky-400 h-4 w-4 animate-pulse" /> */}
+              <p className="text-sm md:text-xl xl:text-lg">
+                {pageInfo.phoneNumber}
+              </p>
+            </div>
 
-          {/* <div className="flex items-center space-x-5 justify-center">
+            {/* <div className="flex items-center space-x-5 justify-center">
             <EnvelopeIcon className="text-sky-400 h-4 w-4 animate-pulse" />
             <p className="text-sm md:text-xl xl:text-lg">{pageInfo.email}</p>
           </div> */}
 
-          {/* <div className="flex items-center space-x-3 justify-center">
+            {/* <div className="flex items-center space-x-3 justify-center">
             <MapPinIcon className="text-sky-400 h-4 w-4 animate-pulse" />
             <p className="text-sm md:text-xl xl:text-lg">{pageInfo.address}</p>
           </div> */}
-        </div>
+          </div>
 
-        {/* <form
+          {/* <form
           onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col  w-fit space-y-1 lg:space-y-3 lg:w-fit"
         >
@@ -93,61 +95,62 @@ function ContactMe({ pageInfo }: Props) {
             Submit
           </button>
         </form> */}
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="max-w-md mx-auto mt-8 p-4 shadow-md rounded-md"
-        >
-          <div className="grid grid-cols-2 gap-1 mb-4">
-            <input
-              type="text"
-              name="name"
-              // value={formValues.name}
-              // onChange={handleChange}
-              placeholder="Name"
-              className="contactInput"
-              required
-            />
-            <input
-              type="email"
-              name="email"
-              // value={formValues.email}
-              // onChange={handleChange}
-              placeholder="Email"
-              className="contactInput"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              type="text"
-              name="subject"
-              // value={formValues.subject}
-              // onChange={handleChange}
-              placeholder="Subject"
-              className="contactInput"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <textarea
-              name="message"
-              // value={formValues.message}
-              // onChange={handleChange}
-              placeholder="Message"
-              rows={4}
-              className="contactInput"
-              required
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 hover:bg-blue-600 text-white py-2 rounded-md"
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="max-w-md mx-auto mt-8 p-4 shadow-md rounded-md"
           >
-            Submit
-          </button>
-        </form>
+            <div className="grid grid-cols-2 gap-1 mb-4">
+              <input
+                type="text"
+                name="name"
+                // value={formValues.name}
+                // onChange={handleChange}
+                placeholder="Name"
+                className="contactInput"
+                required
+              />
+              <input
+                type="email"
+                name="email"
+                // value={formValues.email}
+                // onChange={handleChange}
+                placeholder="Email"
+                className="contactInput"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                type="text"
+                name="subject"
+                // value={formValues.subject}
+                // onChange={handleChange}
+                placeholder="Subject"
+                className="contactInput"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <textarea
+                name="message"
+                // value={formValues.message}
+                // onChange={handleChange}
+                placeholder="Message"
+                rows={4}
+                className="contactInput"
+                required
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 hover:bg-blue-600 text-white py-2 rounded-md"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
